c712dafe80017ceda906746ee7b9a13ac5ab57ed
james lay <james@eventrentalsystems.com>
2023-05-24 12:00:09 -0600

    insert tests pass

--- src/evaluate.js
+++ src/evaluate.js
@@ -399,10 +399,10 @@ function evaluateInsert(database, ast) {
         }
 
         const schema = database.__schema__;
-
         const tname = ast.table[0].table;
+
         // 2nd, check that the table exists:
-        if (typeof schema[table] === undefined) {
+        if (typeof schema[tname] === undefined) {
             throw new Error(`Cannot INSERT into unknown table '${tname}'`);
         }
 
@@ -417,7 +417,7 @@ function evaluateInsert(database, ast) {
             throw new Error("INSERT statements must have exactly ONE set of values.");
         }
         const vals = ast.values[0].value.map(e => e.value);
-        if (vals.length !== ast.values[0]) {
+        if (vals.length !== ast.values[0].value.length) {
             throw new Error("INSERT number of values must match the number of columns.");
         }
 
@@ -428,10 +428,10 @@ function evaluateInsert(database, ast) {
             const col = ast.columns[i];
 
             // check the type match in the schema:
-            if (schema[tname][col] !== typeof val) {
+            if (schema[tname][col].type !== typeof val) {
                 throw new Error(
                     `INSERT value for column '${col}' in table '${tname}' ` +
-                    `is type '${typeof val}' but should be '${schema[tname][col]}'`
+                    `is type '${typeof val}' but should be '${schema[tname][col].type}'`
                 );
             }
 
@@ -441,6 +441,48 @@ function evaluateInsert(database, ast) {
 
         // finally, check constraints:
         // TODO: check constraints
+        // ERS only really uses and therefore supports NOT NULL constraints.
+        for (const col of Object.keys(schema[tname])) {
+
+            // make sure auto_increment fields are numbers:
+            if (schema[tname][col].auto_increment) {
+
+                if (schema[tname][col].type !== "number") {
+                    throw new Error(
+                        `INSERT failed because column '${col}' in table '${tname}' ` +
+                        `is an auto_increment column but is not a number`
+                    );
+                }
+
+                // make sure they're not specifying a value for an auto_increment field:
+                if (insert_row[col] !== undefined) {
+                    throw new Error(
+                        `INSERT failed because column '${col}' in table '${tname}' ` +
+                        `is an auto_increment column but a value was specified`
+                    );
+                }
+
+                if (!insert_row[col]) {
+                    // auto increment the value
+                    // find the max value in the table, and add one.
+                    const newid = database[tname].map(row => row[col]).reduce((a, b) => Math.max(a, b), 0) + 1;
+                    insert_row[col] = newid;
+                }
+
+            }
+
+            if (schema[tname][col].not_null && !insert_row[col]) {
+                throw new Error(
+                    `INSERT value for column '${col}' in table '${tname}' ` +
+                    `cannot be NULL`
+                );
+            }
+
+            // autofill empty values to null
+            if (insert_row[col] === undefined) {
+                insert_row[col] = null;
+            }
+        }
 
         // and insert:
         database[tname].push(insert_row);
